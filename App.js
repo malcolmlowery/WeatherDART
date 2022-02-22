0a1085745b122ff292b2ed69bd6be0e9b4f7924c
Software Development <softwaredevelopment@Softwares-MacBook-Pro.local>
2022-02-22 15:18:21 -0500

    Fixed merge conflicts

--- App.js
+++ App.js
@@ -1,25 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-=======
 import Main from './src/Main';
 
 const App = () => {
@@ -29,4 +7,3 @@ const App = () => {
 }
 
 export default App;
->>>>>>> tmp
