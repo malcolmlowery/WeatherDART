a196a976d32f7b2aab350e5310ecc1acaaec4ede
Software Development <softwaredevelopment@Malcolms-MacBook-Air.local>
2023-09-26 18:33:14 -0400

    Updated the layout for the Dashboard page

--- src/pages/dashboard.page.tsx
+++ src/pages/dashboard.page.tsx
@@ -6,15 +6,13 @@ const Dashboard = () => {
   const { isLoadingMapContent, observationsData, map } =
     useContext(WeatherDataContext);
   const { place, ob } = observationsData;
-  console.log(map);
+
   const city_state_name =
     place &&
     place?.city.charAt(0).toUpperCase() +
       place?.city.slice(1) +
       ', ' +
       place?.state.toUpperCase();
-
-  console.log(map);
 
   return (
     <div className='dashboard'>
@@ -24,12 +22,55 @@ const Dashboard = () => {
           <img src={map} />
           {ob && (
             <div className='dashboard__current-weather__stats'>
-              <p>
-                The temperature in {city_state_name} is {ob?.tempF}°{' '}
-                {ob?.feelslikeF !== ob?.tempF &&
-                  'but feels like ' + ob?.feelslikeF + '°'}{' '}
-                ⛅️
-              </p>
+              <div className='dashboard__current-weather__stats__header'>
+                <h3 className='dashboard__current-weather__stats__header__city-state-name'>
+                  {city_state_name}
+                </h3>
+                <h2 className='dashboard__current-weather__stats__header__temp'>
+                  Currenly {ob.tempF}°
+                </h2>
+              </div>
+
+              <h2 className='dashboard__current-weather__stats__condition-outside'>
+                {ob.weather}
+              </h2>
+
+              <div className='dashboard__current-weather__stats__details'>
+                <div className='dashboard__current-weather__stats__details__left-side'>
+                  <p>
+                    feels like: <b>{ob.feelslikeF}</b>
+                  </p>
+                  <p>
+                    humidity: <b>{ob.humidity}%</b>
+                  </p>
+                  <p>
+                    visibilty: <b>{ob.visibilityMI}%</b>
+                  </p>
+                  <p>
+                    wind speed: <b>{ob.windMPH}mph</b>
+                  </p>
+                  <p>
+                    wind direction: <b>{ob.windDir}</b>
+                  </p>
+                </div>
+                <div className='dashboard__current-weather__stats__details__right-side'>
+                  <p>
+                    feels like: <b>{ob.feelslikeF}</b>
+                  </p>
+                  <p>
+                    humidity: <b>{ob.humidity}%</b>
+                  </p>
+                  <p>
+                    visibilty: <b>{ob.visibilityMI}%</b>
+                  </p>
+                  <p>
+                    wind speed: <b>{ob.windMPH}mph</b>
+                  </p>
+                  <p>
+                    wind direction: <b>{ob.windDir}</b>
+                  </p>
+                </div>
+              </div>
             </div>
           )}
         </div>
