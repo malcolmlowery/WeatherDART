0bab368cb603f01362a9d4a46553633984a51bb1
Software Development <softwaredevelopment@Malcolms-MacBook-Air.local>
2023-09-26 15:34:54 -0400

    Added some styling to Dashboard page

--- src/context/WeatherDataContext.tsx
+++ src/context/WeatherDataContext.tsx
@@ -14,6 +14,8 @@ interface PropsI {
 interface WeatherDataContextI {
   fetchWeatherObervationsData: (a: string) => Promise<void>;
   observationsData: Partial<ObservationDataI>;
+  map: string | undefined;
+  isLoadingMapContent: boolean;
 }
 
 const WeatherDataContext = createContext<WeatherDataContextI>(
@@ -21,30 +23,59 @@ const WeatherDataContext = createContext<WeatherDataContextI>(
 );
 
 const WeatherDataProvider = ({ children }: PropsI) => {
+  const [isLoadingMapContent, setIsLoadingMapContent] = useState(false);
+  const [map, setMap] = useState<string>();
   const [observationsData, setObservationData] = useState<
     ObservationDataI | object
   >({});
 
-  const fetchWeatherObervationsData = async (text: string) => {
-    aeris
-      .api()
-      .endpoint('observations')
-      .place(text)
-      .get()
-      .then(({ data, error }) => {
-        if (error) {
-          return new Error(
-            'An error performing your request. Please try again',
-          );
-        }
-        setObservationData(data);
-      })
-      .catch((error) => console.log(error));
+  const fetchWeatherObervationsData = async (input: string) => {
+    setIsLoadingMapContent(true);
+    try {
+      aeris
+        .api()
+        .endpoint('observations')
+        .place(input)
+        .get()
+        .then(({ data, error }) => {
+          if (error) {
+            return new Error(
+              'An error performing your request. Please try again',
+            );
+          }
+          setObservationData(data);
+        })
+        .catch((error) => console.log(error));
+
+      const aeris_map = aeris.map();
+      aeris_map.layers(
+        'stormcells,satellite-visible,radar,counties,admin-cities,roads',
+      );
+      aeris_map.center(input).zoom(8);
+      aeris_map.size(500, 500);
+
+      aeris_map
+        .get()
+        .then((result) => {
+          if (result.error) {
+            return new Error(
+              'An error performing your request. Please try again',
+            );
+          }
+          setMap(result.image.src);
+          setIsLoadingMapContent(false);
+        })
+        .catch((error) => console.log(error));
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const values = {
     fetchWeatherObervationsData,
     observationsData,
+    map,
+    isLoadingMapContent,
   };
 
   return (
--- src/pages/dashboard.page.tsx
+++ src/pages/dashboard.page.tsx
@@ -1,13 +1,12 @@
 import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
 import './p_styles/dashboard.page.css';
 import { WeatherDataContext } from '../context/WeatherDataContext';
 
 const Dashboard = () => {
-  const { user } = useContext(AuthContext);
-  const { observationsData } = useContext(WeatherDataContext);
+  const { isLoadingMapContent, observationsData, map } =
+    useContext(WeatherDataContext);
   const { place, ob } = observationsData;
-
+  console.log(map);
   const city_state_name =
     place &&
     place?.city.charAt(0).toUpperCase() +
@@ -15,16 +14,25 @@ const Dashboard = () => {
       ', ' +
       place?.state.toUpperCase();
 
+  console.log(map);
+
   return (
     <div className='dashboard'>
-      <h1>Hello!</h1>
-      <p>Signed in with email: {user?.email}</p>
-      {ob && (
-        <p>
-          The temperature in {city_state_name} is {ob?.tempF}°{' '}
-          {ob?.feelslikeF !== ob?.tempF && 'but feels like '}
-          {ob?.feelslikeF !== ob?.tempF && ob?.feelslikeF + '°'} ⛅️
-        </p>
+      {isLoadingMapContent && <h3>Please wait...</h3>}
+      {!isLoadingMapContent && (
+        <div className='dashboard__current-weather'>
+          <img src={map} />
+          {ob && (
+            <div className='dashboard__current-weather__stats'>
+              <p>
+                The temperature in {city_state_name} is {ob?.tempF}°{' '}
+                {ob?.feelslikeF !== ob?.tempF &&
+                  'but feels like ' + ob?.feelslikeF + '°'}{' '}
+                ⛅️
+              </p>
+            </div>
+          )}
+        </div>
       )}
     </div>
   );
